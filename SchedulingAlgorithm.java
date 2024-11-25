b556699d82d116434a3a9f612e60cb926910500d
1481357505 <141431602+1481357505@users.noreply.github.com>
2024-11-25 21:01:36 +0800

    Update SchedulingAlgorithm.java

--- SchedulingAlgorithm.java
+++ SchedulingAlgorithm.java
@@ -1,109 +1,90 @@
-// Run() is called from Scheduling.main() and is where
-// the scheduling algorithm written by the user resides.
-// User modification should occur within the Run() function.
-
 import java.util.*;
 import java.io.*;
 
 public class SchedulingAlgorithm {
 
-  public static Results Run(int runtime, Vector<sProcess> processVector, Results result, int timeSlice) {
+    public static Results Run(int runtime, Vector<sProcess> processVector, Results result, int timeSlice) {
+        String resultsFile = "Summary-Processes";
 
-    String resultsFile = "Summary-Processes";
-
-    try {
-      PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
-      // Call round robin here, after implementing it and remove or comment out the following line.
-      FIFO(runtime, processVector, result, out);
-      //
-      out.close();
-    } catch (IOException e) { /* Handle exceptions */
-      e.printStackTrace();
+        try {
+            PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
+            // Call round robin here
+            roundRobin(runtime, timeSlice, processVector, result, out);
+            // Uncomment the following line to use FIFO instead
+            // FIFO(runtime, processVector, result, out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
-    return result;
-  }
 
-  private static void roundRobin(int runtime, int timeSlice, Vector<sProcess> processVector, Results result, PrintStream out){
-    // ADD YOUR CODE HERE AND ROMOVE THE FOLLOWING LINE
-    throw (new RuntimeException("Round Robin method not implemented."));
-  }
+    private static void roundRobin(int runtime, int timeSlice, Vector<sProcess> processVector, Results result, PrintStream out) {
+        int comptime = 0; // Total elapsed time
+        int size = processVector.size(); // Number of processes
+        int completed = 0; // Number of completed processes
+        int currentProcess = 0; // Index of the current process
 
+        result.schedulingType = "Batch (Preemptive)";
+        result.schedulingName = "Round Robin";
 
-  private static void FIFO(int runtime, Vector<sProcess> processVector, Results result, PrintStream out){
-    int i = 0;
-    int comptime = 0;
-    int currentProcess = 0;
-    int previousProcess = 0;
-    int size = processVector.size();
-    int completed = 0;
+        while (comptime < runtime && completed < size) {
+            sProcess process = (sProcess) processVector.elementAt(currentProcess);
 
-    result.schedulingType = "Batch (Nonpreemptive)";
-    result.schedulingName = "First-Come First-Served";
-
-    try {
-      sProcess process = (sProcess) processVector.elementAt(currentProcess);
-      printRegistered(out, currentProcess, process, comptime);
-      while (comptime < runtime) {
-
-        // Check completion of the process
-        if (process.cpudone == process.cputime) {
-          completed++;
-          printCompleted(out, currentProcess, process, comptime);
-          if (completed == size) {
-            result.compuTime = comptime;
-            return;
-          }
-          // scheduling the next process
-          for (i = size - 1; i >= 0; i--) {
-            process = (sProcess) processVector.elementAt(i);
-            if (process.cpudone < process.cputime) {
-              currentProcess = i;
+            // If the process is already completed, skip to the next one
+            if (process.cpudone >= process.cputime) {
+                currentProcess = (currentProcess + 1) % size;
+                continue;
             }
-          }
-          process = (sProcess) processVector.elementAt(currentProcess);
-          printRegistered(out, currentProcess, process, comptime);
 
-        }
-        // Checking for blocking time
-        if (process.ioblocking == process.ionext) {
-          printIOBlocked(out, currentProcess, process, comptime);
-          process.numblocked++;
-          process.ionext = 0;
-          
-          // scheduling the next process
-          previousProcess = currentProcess;
-          for (i = size - 1; i >= 0; i--) {
-            process = (sProcess) processVector.elementAt(i);
-            if (process.cpudone < process.cputime && previousProcess != i) {
-              currentProcess = i;
+            // Print the registered process
+            printRegistered(out, currentProcess, process, comptime);
+
+            // Determine how much time the process can run
+            int timeToRun = Math.min(timeSlice, process.cputime - process.cpudone);
+
+            for (int time = 0; time < timeToRun; time++) {
+                // Simulate the process running
+                process.cpudone++;
+                comptime++;
+
+                // Check for I/O blocking
+                if (process.ioblocking > 0 && process.ionext < process.ioblocking) {
+                    process.ionext++;
+                    if (process.ionext == process.ioblocking) {
+                        printIOBlocked(out, currentProcess, process, comptime);
+                        process.numblocked++;
+                        process.ionext = 0; // Reset I/O next for the next time
+                        break; // Exit the loop to switch processes
+                    }
+                }
+
+                // If the process completes during its time slice
+                if (process.cpudone >= process.cputime) {
+                    completed++;
+                    printCompleted(out, currentProcess, process, comptime);
+                    break; // Exit the loop to switch processes
+                }
             }
-          }
-          process = (sProcess) processVector.elementAt(currentProcess);
-          printRegistered(out, currentProcess, process, comptime);
+
+            // Move to the next process in a circular manner
+            currentProcess = (currentProcess + 1) % size;
         }
-        // increment timer counters
-        process.cpudone++;
-        if (process.ioblocking > 0) {
-          process.ionext++;
+
+        if (completed < size) {
+            out.println("Not enough time to complete all processes!");
         }
-        comptime++;
-      }
-      out.println("Not enough time to complete all processes!");
-    } catch (Exception e) {
-      result.compuTime = comptime;
-      throw(e);
     }
-  }
 
-  private static void printRegistered(PrintStream out, int currentProcess, sProcess process, int comptime){
-    out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
-  }
+    private static void printRegistered(PrintStream out, int currentProcess, sProcess process, int comptime) {
+        out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
+    }
 
-  private static void printCompleted(PrintStream out, int currentProcess, sProcess process, int comptime){
-    out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
-  }
+    private static void printCompleted(PrintStream out, int currentProcess, sProcess process, int comptime) {
+        out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
+    }
 
-  private static void printIOBlocked(PrintStream out, int currentProcess, sProcess process, int comptime){
-    out.println("Process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
-  }
+    private static void printIOBlocked(PrintStream out, int currentProcess, sProcess process, int comptime) {
+        out.println("Process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + comptime + ")");
+    }
 }
